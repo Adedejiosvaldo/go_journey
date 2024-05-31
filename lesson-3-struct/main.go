c8ec1877e34f705ce863aefa774327af247b340a
Joseph Adedeji <adedejiosvaldo@gmail.com>
2024-05-31 11:43:16 +0100

    Struct - Declaring a struct

--- lesson-3-struct/main.go
+++ lesson-3-struct/main.go
@@ -1,10 +1,16 @@
 package main
 
+import "fmt"
+
 type Person struct {
 	firstName string
 	lastName  string
 }
 
 func main() {
+	alex := Person{"Alex", "Anderson"}
+
+	okc := Person{firstName: alex.lastName, lastName: alex.lastName}
+	fmt.Println(okc)
 
 }
