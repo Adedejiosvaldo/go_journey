9fb6ae32ca5c15376d9ceee07364ecf56d2d9dba
Joseph Adedeji <adedejiosvaldo@gmail.com>
2024-05-31 20:58:50 +0100

    Struct  - Adding pointers the shorter way

--- lesson-3-struct/main.go
+++ lesson-3-struct/main.go
@@ -17,10 +17,15 @@ func main() {
 	// Embedding a struct
 	alex := Person{firstName: "Alex", lastName: "OnGod", contactInfo: contactInfo{email: "Adedej", zip: 10000}}
 
-	jimPointer := &alex
-	jimPointer.updateName("Jimmy")
-	alex.print()
+	// Memeory reference to the alwx struct in memeory
+	// Long approach
+	// jimPointer := &alex
+	// jimPointer.updateName("Jimmy")
+	// alex.print()
 
+	// Implementing a shortcut
+	alex.updateName("Jimmy")
+	alex.print()
 	// okc := Person{firstName: alex.lastName, lastName: alex.lastName}
 
 	// Upating a struct
@@ -31,6 +36,8 @@ func (p Person) print() {
 	fmt.Printf("%+v", p)
 }
 
+// Can be called by pointer with a type of perso
 func (pointerToPerson *Person) updateName(firstName string) {
+	// This is the operation to change the actual struct
 	(*pointerToPerson).firstName = firstName
 }
