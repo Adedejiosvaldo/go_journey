a88f8fb5bd84710532a42e1550485644966df466
Joseph Adedeji <adedejiosvaldo@gmail.com>
2024-06-01 20:52:44 +0100

    Maps - Iterating over a map

--- lesson-4-map/main.go
+++ lesson-4-map/main.go
@@ -3,15 +3,26 @@ package main
 import "fmt"
 
 func main() {
-	// colors := map[string]string{
-	// 	"red": "0xxhhrr",
-	// }
-	// fmt.Println(colors)
+	colors := map[string]string{
+		"red": "0xxhhrr",
+	}
+
+	printKeys(colors)
 
 	//  Manipulating Maps
 	// var colorsForMap
 	colorsForMap := make(map[string]string)
 	colorsForMap["red"] = "0x0000"
 
-	fmt.Println(colorsForMap)
+	// Deletes
+	delete(colorsForMap, "red")
+
+	// fmt.Println(colorsForMap)
+}
+
+func printKeys(m map[string]string) {
+
+	for color, card := range m {
+		fmt.Print("Key: " + color + ", value " + card)
+	}
 }
