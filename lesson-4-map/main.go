69bfc7ed7623e8261ab8650a06d48c2ce85bbf70
Joseph Adedeji <adedejiosvaldo@gmail.com>
2024-06-01 20:43:19 +0100

    Maps - Manipulating maps

--- lesson-4-map/main.go
+++ lesson-4-map/main.go
@@ -3,8 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	colors := map[string]string{
-		"red": "0xxhhrr",
-	}
-	fmt.Println(colors)
+	// colors := map[string]string{
+	// 	"red": "0xxhhrr",
+	// }
+	// fmt.Println(colors)
+
+	//  Manipulating Maps
+	// var colorsForMap
+	colorsForMap := make(map[string]string)
+	colorsForMap["red"] = "0x0000"
+
+	fmt.Println(colorsForMap)
 }
