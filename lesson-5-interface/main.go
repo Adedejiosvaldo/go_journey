12e5b1715ea6b0f0e64d691eb37c3a037ae2859f
Joseph Adedeji <adedejiosvaldo@gmail.com>
2024-06-03 13:53:12 +0100

    Interface - Added Bots without interface

--- lesson-5-interface/main.go
+++ lesson-5-interface/main.go
@@ -9,7 +9,18 @@ type spanishBot struct {
 }
 
 func main() {
-	fmt.Println("Hello")
+	eb := englishBot{}
+	sb := spanishBot{}
+	printGreeting(eb)
+	printGreeting(sb)
+}
+
+func printGreeting(eb englishBot) {
+	fmt.Println(eb.getGreeting())
+}
+
+func printGreeting(sb spanishBot) {
+	fmt.Println(sb.getGreeting())
 }
 
 func (eb englishBot) getGreeting() string {
