e723ae3882475f85f2249340b3272f77595be26a
Adewunmi Joseph Adedeji <Adedejiosvaldo@gmail.com>
2024-06-18 16:40:58 +0100

    Interfaces - Assigning commoon variables and Methods

--- full-timego/interfaces/main.go
+++ full-timego/interfaces/main.go
@@ -1,9 +1,38 @@
 package main
 
-type Storer interface{
-    
+import "fmt"
+
+type NumberStorer interface {
+	GetAll() ([]int, error)
+	Put() error
+}
+
+type MongoDBNumberStore struct {
+}
+
+func (m MongoDBNumberStore) GetAll() ([]int, error) {
+	return []int{1, 2, 3, 4}, nil
+}
+func (m MongoDBNumberStore) Put() error {
+	fmt.Println("Printling ")
+	return nil
+}
+
+type ApiServer struct {
+	numberStore NumberStorer
 }
 
 func main() {
+
+	apiServer := ApiServer{
+		numberStore: MongoDBNumberStore{},
+	}
+
+	number, err := apiServer.numberStore.GetAll()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(number)
 
 }
